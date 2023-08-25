3617e299b5d1cabb9635950e0388816afc7f4d0d
enzobitencourt <enzogbitencourt@gmail.com>
2023-08-24 22:54:05 -0300

    teste Carrossel certo

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,36 +1,70 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import './CustomCarousel.css';
-import Object from '../../Cards/ObjectCarousel/Object';
+import styled from 'styled-components';
+import Object from '../../Cards/ObjectCarousel/Object'
+
+const StyledCarousel = styled(Carousel)`
+  .carousel-slider {
+    max-width: 100vw; /* Largura máxima dos slides */
+    margin: 0 auto;
+  }
+
+  .slide {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    background-color: #f5f5f5;
+    border: 1px solid #ccc;
+    padding: 20px;
+    box-sizing: border-box;
+    height: 300px;
+  }
+
+  .control-dots {
+    display: flex;
+    justify-content: center;
+    margin-top: 10px;
+    
+    .dot {
+      background-color: #ccc;
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      margin: 0 5px;
+      cursor: pointer;
+    }
+
+    .dot.selected {
+      background-color: #333;
+    }
+  }
+`;
 
 const CustomCarousel = () => {
-    return (
-        <>
-            <div className="carousel-wrapper">
-                <Carousel
-                    showThumbs={false}
-                    showStatus={false}
-                    showIndicators={true}
-                    infiniteLoop={true}
-                    transitionTime={500}
-                >
-                    <div className="carousel-slide">
-                        <h2>Slide 1</h2>
-                        <p>Conteúdo do slide 1</p>
-                    </div>
-                    <div className="carousel-slide">
-                        <h2>Slide 2</h2>
-                        <p>Conteúdo do slide 2</p>
-                    </div>
-                    <div className="carousel-slide">
-                        <h2>Slide 3</h2>
-                        <p>Conteúdo do slide 3</p>
-                    </div>
-                </Carousel>
-            </div>
-        </>
-    );
+  return (
+    <StyledCarousel
+      showThumbs={false}
+      showStatus={false}
+      showArrows={false}
+      showIndicators={true}
+      infiniteLoop={true}
+      transitionTime={500}
+    >
+      <div className="slide">
+            <Object/>
+      </div>
+      <div className="slide">
+            <Object/>
+      </div>
+      <div className="slide">
+            <Object/>
+      </div>
+
+    </StyledCarousel>
+  );
 };
 
 export default CustomCarousel;
