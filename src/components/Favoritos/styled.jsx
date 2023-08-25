49c3e6ea678c67507479f9c9cd8f5e1149ec4ba5
enzobitencourt <enzogbitencourt@gmail.com>
2023-08-25 13:08:11 -0300

    democratize

--- src/components/Favoritos/styled.jsx
+++ src/components/Favoritos/styled.jsx
@@ -9,5 +9,17 @@ export const ContainerFav = styled.div`
     height: auto;
     margin-bottom: 2vh;
     margin-top: 2vh;
+
+    ::-webkit-scrollbar {
+    display: none;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: rgba(0, 0, 0, 0);
+}
+
+::-webkit-scrollbar-track {
+  background-color: rgba(0, 0, 0, 0);
+}
 `
 
