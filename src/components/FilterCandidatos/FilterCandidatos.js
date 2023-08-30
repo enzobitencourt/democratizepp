7d060dcb26507f10d97106b34a2fc95e99b3084e
enzobitencourt <enzogbitencourt@gmail.com>
2023-08-30 18:08:16 -0300

    democratize

--- src/components/FilterCandidatos/FilterCandidatos.js
+++ src/components/FilterCandidatos/FilterCandidatos.js
@@ -2,37 +2,53 @@ import {
     ModalOverlay,
     ModalContent,
     ModalHeader,
+    Modal,
+    useDisclosure,
     Checkbox
 } from '@chakra-ui/react'
 
-import { Direita, Esquerda, Filters } from './styled'
+import { Direita, Esquerda, Filters, Input, DivInput, Enviar} from './styled'
+import { InfoIcon } from '@chakra-ui/icons'
 
 function FilterCandidatos() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
         <>
             <ModalOverlay />
-            <ModalContent padding='0.5vh 1vh 3vh 1vh' w='80vw' borderRadius='28.4px' background='#FFF' gap='1.5vh'>
-                <ModalHeader>Filtre por palavras-chave</ModalHeader>
+            <ModalContent display='flex' padding='0.5vh 1vh 3vh 1vh' w='83vw' borderRadius='28.4px' background='#FFF' gap='1.5vh' alignItems='center'>
+                <ModalHeader fontSize='18px' gap='2vw' display='flex' flexDirection='row' alignItems='center'>
+                    <p>Filtre por palavras-chave</p>
+                    <InfoIcon onClick={onOpen} color='#1B676B' />
+                    <Modal isOpen={isOpen} onClose={onClose}>
+                        <ModalOverlay />
+                        <ModalContent w='50vw' padding='23px 0px 22px 7px' borderRadius='18.2px'>
+                            Filtre os candidatos conforme palavras-chave nas suas propostas de mandato
+                        </ModalContent>
+                    </Modal>
+                </ModalHeader>
                 <Filters>
                     <Direita>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
+                        <Checkbox colorScheme='green'>Animais</Checkbox>
+                        <Checkbox colorScheme='green'>Meio-ambiente</Checkbox>
+                        <Checkbox colorScheme='green'>Educação</Checkbox>
+                        <Checkbox colorScheme='green'>Reforma Agrária</Checkbox>
+                        <Checkbox colorScheme='green'>Segurança</Checkbox>
+                        <Checkbox colorScheme='green'>Saúde</Checkbox>
                     </Direita>
                     <Esquerda>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
-                        <Checkbox colorScheme='green'>Checkbox</Checkbox>
+                        <Checkbox colorScheme='green'>Feminismo</Checkbox>
+                        <Checkbox colorScheme='green'>Igualdade</Checkbox>
+                        <Checkbox colorScheme='green'>Economia</Checkbox>
+                        <Checkbox colorScheme='green'>Empreendedor</Checkbox>
+                        <Checkbox colorScheme='green'>Salário</Checkbox>
+                        <Checkbox colorScheme='green'>Previdência</Checkbox>
                     </Esquerda>
                 </Filters>
+                <DivInput>
+                        <Input placeholder='Digite palavras-chave'/>
+                        <Enviar>Enviar</Enviar>
+                </DivInput>
             </ModalContent>
         </>
     )
--- src/components/FilterCandidatos/styled.jsx
+++ src/components/FilterCandidatos/styled.jsx
@@ -6,13 +6,14 @@ export const Filters = styled.div`
     width: 80vw;
     justify-content: space-between;
     padding: 0vh 1vh;
+    margin-bottom: 2vh;
 `
 
 export const Direita = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1.5vh;
-    width: 37vw;
+    width: 40vw;
     height: auto;
 `
 
@@ -20,6 +21,31 @@ export const Esquerda = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1.5vh;
-    width: 37vw;
+    width: 35vw;
     height: auto;
+`
+
+export const DivInput = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: center;
+    height: 3vh;
+`
+
+export const Input = styled.input`
+    width: 55vw;
+    height: 4vh;
+    border-radius: 28.6px 0 0 28.6px;
+    background-color: white;
+    padding: 0 0 0 2vw;
+    border: 1px solid black;
+    font-size: 15px;
+`
+
+export const Enviar = styled.button`
+    width: 17vw;
+    height: 4vh;
+    border: 1px solid #1B676B;
+    color: #1B676B;
 `
