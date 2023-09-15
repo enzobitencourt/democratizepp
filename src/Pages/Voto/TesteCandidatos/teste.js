df3e4bb4110e1d27e700ab314673eac990a7527f
enzobitencourt <enzogbitencourt@gmail.com>
2023-09-15 15:09:31 -0300

    teste que deu certo

--- src/Pages/Voto/TesteCandidatos/teste.js
+++ src/Pages/Voto/TesteCandidatos/teste.js
@@ -2,18 +2,7 @@ import React, { useState, useEffect } from 'react';
 import * as pdfjs from 'pdfjs-dist';
 import 'pdfjs-dist/build/pdf.worker.entry';
 import { styled } from 'styled-components';
-import BR280001734029 from '../pdfs/2022BR280001734029.pdf'
-import BR280001607833 from '../pdfs/2022BR280001607833.pdf'
-import BR280001600167 from '../pdfs/2022BR280001600167.pdf'
-import BR280001602702 from '../pdfs/2022BR280001602702.pdf'
-import BR280001677435 from '../pdfs/2022BR280001677435.pdf'
-import BR280001612393 from '../pdfs/2022BR280001612393.pdf'
-import BR280001607831 from '../pdfs/2022BR280001607831.pdf'
-import BR280001603612 from '../pdfs/2022BR280001603612.pdf'
-import BR280001618036 from '../pdfs/2022BR280001618036.pdf'
-import BR280001607829 from '../pdfs/2022BR280001607829.pdf'
-import BR280001644128 from '../pdfs/2022BR280001644128.pdf'
-import candidatos from '../../../LogicaCandidatos/database.json'
+import candidatos from '../../../LogicaCandidatos/database.json';
 
 const PdfTextExtractor = () => {
     const Div = styled.div`
@@ -21,21 +10,16 @@ const PdfTextExtractor = () => {
         height: auto;
         display: flex;
         flex-direction: column;
-    `
+    `;
 
-    const [pdfText, setPdfText] = useState('');
-    const [containsKeyword, setContainsKeyword] = useState(false);
-
+    const [pdfResults, setPdfResults] = useState([]);
     const keyword = 'economia';
 
-    const extractTextFromPdf = async (filePath) => {
+    const extractTextFromPdf = async (pdfData, candidate) => {
         try {
-            const response = await fetch(filePath);
-            const arrayBuffer = await response.arrayBuffer();
-            const pdfData = new Uint8Array(arrayBuffer);
             pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.js';
 
-            const loadingTask = pdfjs.getDocument({ data: pdfData });
+            const loadingTask = pdfjs.getDocument(pdfData);
             const pdf = await loadingTask.promise;
 
             let fullText = '';
@@ -47,39 +31,44 @@ const PdfTextExtractor = () => {
                 fullText += pageText;
             }
 
-            setPdfText(fullText);
+            const containsKeyword = fullText.includes(keyword);
 
-            if (fullText.includes(keyword)) {
-                setContainsKeyword(true);
-            } else {
-                setContainsKeyword(false);
-            }
+            return { candidate, containsKeyword };
         } catch (error) {
-            console.error('Erro ao extrair texto do PDF:', error);
+            console.error(`Error extracting text from PDF for candidate ${candidate.nome}:`, error);
+            return { candidate, containsKeyword: false };
         }
     };
 
-    // Use useEffect para carregar o PDF ao montar o componente (substitua 'caminho_do_seu_arquivo.pdf' pelo caminho real do arquivo)
     useEffect(() => {
-        const filePath = BR280001607833;
-        extractTextFromPdf(filePath);
+        const loadPDFs = async () => {
+            const pdfPromises = candidatos.map(async (candidate) => {
+                try {
+                    const module = await import(`../pdfs/2022${candidate.pdf}.pdf`);
+                    const result = await extractTextFromPdf(module.default, candidate);
+                    return result;
+                } catch (error) {
+                    console.error('Error loading dynamic component:', error);
+                    return { candidate, containsKeyword: false };
+                }
+            });
+
+            const results = await Promise.all(pdfPromises);
+            setPdfResults(results);
+        };
+
+        loadPDFs();
     }, []);
 
     return (
         <Div>
             <h2>Extrair Texto de PDF</h2>
-            {pdfText && (
-                <div>
-                    <h3>Texto Extraído:</h3>
-                    <p>{pdfText}</p>
+            {pdfResults.map((result, index) => (
+                <div key={index}>
+                    <h3>{result.candidate.nome}</h3>
+                    <p>Contains Keyword: {result.containsKeyword.toString()}</p>
                 </div>
-            )}
-            {containsKeyword && (
-                <div>
-                    <h3>Contém a palavra-chave:</h3>
-                    <p>true</p>
-                </div>
-            )}
+            ))}
         </Div>
     );
 };
